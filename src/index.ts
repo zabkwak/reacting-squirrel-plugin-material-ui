385d914f06df11434e1082ce9f32fbdf379cc67e
Lukáš Mačuda <lukas.macuda@gmail.com>
2020-11-27 16:27:45 +0100

    fixed loading themes

--- src/index.ts
+++ src/index.ts
@@ -84,8 +84,8 @@ export default class MaterialUIPlugin extends Plugin {
 			const themePath = this._getThemePath(server.appDirAbsolute);
 			if (themePath) {
 				const themeImport = path.relative(rsDir, themePath).replace(/\\/g, '/');
-				const m = '../../..';
-				// const m = 'reacting-squirrel-plugin-material-ui';
+				// const m = '../../..';
+				const m = 'reacting-squirrel-plugin-material-ui';
 				fs.writeFileSync(path.resolve(rsDir, 'mui.js'), `import ThemeProvider from '${m}/dist/theme-provider';
 
 import theme from '${themeImport}';
